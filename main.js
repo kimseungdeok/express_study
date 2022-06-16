c7628fa7e50767a4da0632d09c4429aea26223f7
KimSeungDeok <tmdejr1117@gmail.com>
2022-06-16 23:27:01 +0900

    express 학습

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ var path = require('path');
 var sanitizeHtml = require('sanitize-html');
 var qs = require('querystring');
 var bodyParser = require('body-parser');
-var compression = require('compression')
+var compression = require('compression');
+var topicRouter = require('./routes/topic');
 const express = require('express');
 const req = require('express/lib/request');
 const { response } = require('express');
@@ -20,6 +21,8 @@ app.get('*', function(request,response,next){
     next();
   });
 });
+
+app.use('/topic', topicRouter);
 
 // route, routing : 방향을 잡는것
 // app.get('/', (req, res) => {res.send('Hello World!')})
@@ -38,105 +41,7 @@ app.get('/', function(request, response){
   
 });
 
-app.get('/topic/create', function(request,response){
-  var title = 'WEB - create';
-  var list = template.list(request.list);
-  var html = template.HTML(title, list, `
-    <form action="/topic/create_process" method="post">
-      <p><input type="text" name="title" placeholder="title"></p>
-      <p>
-        <textarea name="description" placeholder="description"></textarea>
-      </p>
-      <p>
-        <input type="submit">
-      </p>
-    </form>
-  `, '');
-  response.send(html);
-});
 
-app.post('/topic/create_process', function(request, response){
-  var post = request.body;
-  var title = post.title;
-  var description = post.description;
-  fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-
-    response.redirect(`/topic/${title}`);
-  })
-});
-
-app.get('/topic/update/:pageid', function(request, response){
-  var filteredId = path.parse(request.params.pageid).base;
-  fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-    var title = request.params.pageid;
-    var list = template.list(request.list);
-    var html = template.HTML(title, list,
-      `
-      <form action="/topic/update_process" method="post">
-        <input type="hidden" name="id" value="${title}">
-        <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-        <p>
-          <textarea name="description" placeholder="description">${description}</textarea>
-        </p>
-        <p>
-          <input type="submit">
-        </p>
-      </form>
-      `,
-      `<a href="/topic/create">create</a> <a href="/topic/update${title}">update</a>`
-    );
-    response.send(html);
-  });
-});
-
-app.post('/topic/update_process', function(request,response){
-var post = request.body;
-    var id = post.id;
-    var title = post.title;
-    var description = post.description;
-    fs.rename(`data/${id}`, `data/${title}`, function(error){
-      fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-        response.redirect(`/${title}`);
-      })
-    });
-});
-
-app.post('/topic/delete_process', function(request,response){
-  var post = request.body;
-        var id = post.id;
-        var filteredId = path.parse(id).base;
-        fs.unlink(`data/${filteredId}`, function(error){
-          response.redirect('/')
-        })
-  });
-
-app.get('/topic/:pageid', function(request, response, next){
-  var filteredId = path.parse(request.params.pageid).base;
-  fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-    if(err){
-      next(err);
-    } else {
-      var title = request.params.pageid;
-      var sanitizedTitle = sanitizeHtml(title);
-      var sanitizedDescription = sanitizeHtml(description, {
-        allowedTags:['h1']
-      });
-      var list = template.list(request.list);
-      var html = template.HTML(sanitizedTitle, list,
-        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/topic/create">create</a>
-          <a href="/topic/update/${sanitizedTitle}">update</a>
-          <form action="/topic/delete_process" method="post">
-            <input type="hidden" name="id" value="${sanitizedTitle}">
-            <input type="submit" value="delete">
-          </form>`
-      );
-      response.send(html);
-    }
-
-  });
-
-});
 
 
 app.use(function(req, res, next) {
